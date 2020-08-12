be74a5124a96185e37e91a0f83cb6bc1ddbcb218
cuongtdc <duycuongtdc@gmail.com>
2020-08-12 19:11:35 +0700

    no message

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+
+app.set('views', `${__dirname}/./views`);
+app.set('view engine', 'ejs');
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('index', {})
 });
 app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening on port 3000!');
